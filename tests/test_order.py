a42e72868c6b68d85255a4116601f22d70cf788f
Алексей <denaz2020@mail.ru>
2025-09-22 23:53:01 +0800

    add доп. задание (order, warehouse, test_order)

---
+++ tests/test_order.py
@@ -0,0 +1,18 @@
+from src.order import Order
+from src.main_16_2 import Product
+
+
+def test_order_init(product: Product) -> None:
+    """Тестируем инициализацию заказа"""
+    order = Order(product, 2)
+    assert order.product.name == "Samsung Galaxy S23 Ultra"
+    assert order.quantity == 2
+    assert order.total_price == 360000
+
+
+def test_order_str(product: Product) -> None:
+    """Тестируем строковое отображение заказа"""
+    order = Order(product, 2)
+    assert str(order) == (
+        'Заказан товар "Samsung Galaxy S23 Ultra" в количестве 2 шт.\n' "Сумма к оплате 360000.00 руб."
+    )
