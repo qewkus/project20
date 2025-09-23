7ee66439c83a97fbbeb6e56a3c54d96604b338d3
Алексей <denaz2020@mail.ru>
2025-09-23 22:04:13 +0800

    добавлены main все + проверка линтерами

---
+++ main_14_1.py
@@ -0,0 +1,65 @@
+import os
+
+from src.category import Category
+from src.product import Product
+from src.utils import create_objects_from_json, read_json
+
+if __name__ == "__main__":
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    path_file = os.path.join(base_dir, "data", "products.json")
+
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
+    print(product1.price)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
+    print(product3.price)
+    print(product3.quantity)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
+
+    products_data = read_json(path_file)
+    categories_list = create_objects_from_json(products_data)
+
+    print(categories_list)
+    print(categories_list[0].name)
+    print(categories_list[0].products)
+
+    print(categories_list[1].name)
+    print(categories_list[1].products)
